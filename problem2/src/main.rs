349bead30451bcfe2aaf8762adafbf9596a4e834
Charlie Saunders <charlieasaunders@gmail.com>
2018-12-12 20:10:42 -0500

    Adds hamming distance function, working towards problem 2.

--- problem2/src/main.rs
+++ problem2/src/main.rs
@@ -39,7 +39,55 @@ fn part1(input: &str) -> Result<()> {
     Ok(())
 }
 
+/*
+The boxes will have IDs which differ by exactly one character at the same position in both strings. For example, given the following box IDs:
+
+abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz
+
+The IDs abcde and axcye are close, but they differ by two characters (the second and fourth).
+
+However, the IDs fghij and fguij differ by exactly one character, the third (h and u). Those must be the correct boxes.
+
+What letters are common between the two correct box IDs? (In the example above, this is found by removing the differing character from either ID, producing fgij.)
+*/
 fn part2(_input: &str) -> Result<()> {
     println!("Not implemented");
     Ok(())
+}
+
+fn hamming_distance(s1: &str, s2: &str) -> i32 {
+    let mut distance = 0;
+    for (s1_char, s2_char) in s1.chars().zip(s2.chars()) {
+        if s1_char != s2_char {
+            distance = distance + 1;
+        }
+    }
+
+    distance
+}
+
+#[cfg(test)]
+mod tests {
+    use super::hamming_distance;
+
+    #[test]
+    fn empty() {
+        assert_eq!(hamming_distance("", ""), 0)
+    }
+
+    #[test]
+    fn equal() {
+        assert_eq!(hamming_distance("aaa", "aaa"), 0)
+    }
+
+    #[test]
+    fn not_equal() {
+        assert_eq!(hamming_distance("aaa", "aba"), 1)
+    }
 }
