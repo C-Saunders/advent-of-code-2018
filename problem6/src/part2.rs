0d1b9312d0352b63be496dfb6d44f619c354c9d9
Charlie Saunders <charlieasaunders@gmail.com>
2019-01-01 15:44:17 -0500

    Corrects max area calculation for problem 6 part 2.

--- problem6/src/part2.rs
+++ problem6/src/part2.rs
@@ -5,20 +5,23 @@ pub fn part2(node_list: &NodeList) -> () {
     let total_area = calculate_max_possible_area(node_list, MAX_DISTANCE);
     println!("Part 2 search total area = {:?}", total_area);
     let grid = calculate_distances(&node_list, &total_area);
+    let target_area = find_area(&grid, MAX_DISTANCE);
+    assert_eq!(target_area, 45290);
     println!("Part 2 area = {}", find_area(&grid, MAX_DISTANCE));
 }
 
 fn calculate_max_possible_area(node_list: &NodeList, max_distance: i32) -> Area {
     let bounding_area = Area::from_node_list(node_list);
+    let node_count = node_list.len();
 
     let min_x =
-        ((bounding_area.max_x() - max_distance) as f32 / node_list.len() as f32).floor() as i32;
+        bounding_area.min_x() - ((max_distance as f32 / node_count as f32)).floor() as i32;
     let min_y =
-        ((bounding_area.max_y() - max_distance) as f32 / node_list.len() as f32).floor() as i32;
+        bounding_area.min_y() - ((max_distance as f32 / node_count as f32)).floor() as i32;
     let max_x =
-        ((bounding_area.min_x() + max_distance) as f32 / node_list.len() as f32).floor() as i32;
+        bounding_area.max_x() + ((max_distance as f32 / node_count as f32)).floor() as i32;
     let max_y =
-        ((bounding_area.min_y() + max_distance) as f32 / node_list.len() as f32).floor() as i32;
+        bounding_area.max_y() + ((max_distance as f32 / node_count as f32)).floor() as i32;
 
     Area::new(min_x, min_y, max_x, max_y)
 }
