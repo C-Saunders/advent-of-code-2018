04ec19258147305f5d7acc0114340bd9cb7d1014
Charlie Saunders <charlieasaunders@gmail.com>
2019-01-03 21:17:45 -0500

    Completes problem 7.

--- problem7/src/main.rs
+++ problem7/src/main.rs
@@ -6,9 +6,17 @@ mod parser;
 fn main() -> Result<(), String> {
     let input = fs::read_to_string("input/data.txt").map_err(|e| e.to_string())?;
     part1(&input);
+    part2(&input);
     Ok(())
 }
 
 fn part1(input: &str) {
-    println!("{}", parser::Graph::new(input).find_in_order());
+    println!("Part 1 = {}", parser::Graph::new(input).find_in_order());
+}
+
+fn part2(input: &str) {
+    println!(
+        "Part 2 = {:?}",
+        parser::Graph::new(input).find_in_order_with_durations()
+    );
 }
--- problem7/src/parser.rs
+++ problem7/src/parser.rs
@@ -47,6 +47,68 @@ impl FromStr for Edge {
     }
 }
 
+type TimeRemaining = u32;
+
+struct StepsInProgress(HashMap<StepName, TimeRemaining>);
+
+impl StepsInProgress {
+    fn new() -> Self {
+        StepsInProgress(HashMap::with_capacity(5))
+    }
+
+    fn complete_steps(&mut self, time_jump: TimeRemaining) -> Vec<StepName> {
+        for remaining_time in self.0.values_mut() {
+            *remaining_time -= time_jump
+        }
+
+        let completed: Vec<StepName> = self
+            .0
+            .iter()
+            .filter(|(_, time_remaining)| **time_remaining == 0)
+            .map(|(step_name, _)| *step_name)
+            .collect();
+
+        for step in completed.iter() {
+            self.0.remove(step);
+        }
+
+        completed
+    }
+
+    fn smallest_time_remaining(&self) -> u32 {
+        *self
+            .0
+            .values()
+            .fold(None, |min, curr| match min {
+                None => Some(curr),
+                Some(existing) => Some(if curr < existing { curr } else { existing }),
+            })
+            .unwrap_or(&0)
+    }
+
+    fn step_in_progress(&self, step_name: StepName) -> bool {
+        self.0.keys().any(|key| key == &step_name)
+    }
+
+    fn replace_step(
+        &mut self,
+        old_step: StepName,
+        new_step: StepName,
+        new_step_duration: TimeRemaining,
+    ) {
+        self.0.remove(&old_step);
+        self.start_step(new_step, new_step_duration);
+    }
+
+    fn start_step(&mut self, new_step: StepName, new_step_duration: TimeRemaining) {
+        self.0.insert(new_step, new_step_duration);
+    }
+
+    fn num_available_workers(&self) -> usize {
+        5 - self.0.keys().len()
+    }
+}
+
 pub struct Graph(HashMap<StepName, PriorSteps>);
 
 impl Graph {
@@ -92,6 +154,78 @@ impl Graph {
         }
 
         String::from_iter(completed_steps)
+    }
+
+    pub fn find_in_order_with_durations(&self) -> (String, u32) {
+        let mut completed_steps: Vec<StepName> = vec![];
+        let total_steps = self.0.keys().len();
+        let mut in_progress = StepsInProgress::new();
+        let mut elapsed_time = 0;
+
+        while completed_steps.len() < total_steps {
+            let time_jump = in_progress.smallest_time_remaining();
+            elapsed_time += time_jump;
+
+            let mut just_completed = in_progress.complete_steps(time_jump);
+            just_completed.sort_unstable();
+            completed_steps.append(&mut just_completed.clone());
+
+            let mut available_steps: Vec<&StepName> = self
+                .0
+                .iter()
+                .filter(|(step_name, prior_steps)| {
+                    !completed_steps.contains(step_name)
+                        && !in_progress.step_in_progress(**step_name)
+                        && prior_steps.iter().all(|s| completed_steps.contains(s))
+                })
+                .map(|(step_name, _)| step_name)
+                .collect();
+
+            available_steps.sort_unstable();
+
+            if available_steps.is_empty() {
+                continue;
+            }
+
+            let mut available_steps_iter = available_steps.iter();
+
+            // Fill open slots
+            for _ in 1..=in_progress.num_available_workers() {
+                if let Some(available) = available_steps_iter.next() {
+                    in_progress.start_step(**available, get_step_duration(**available));
+                } else {
+                    break;
+                }
+            }
+
+            // Replace completed steps
+            for (completed, available) in just_completed.iter().zip(available_steps_iter) {
+                in_progress.replace_step(*completed, **available, get_step_duration(**available));
+            }
+        }
+
+        (String::from_iter(completed_steps), elapsed_time)
+    }
+}
+
+fn get_step_duration(name: StepName) -> TimeRemaining {
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .char_indices()
+        .find(|(_, ch)| ch == &name)
+        .unwrap()
+        .0 as u32
+        + 61
+}
+
+#[cfg(test)]
+mod test_get_step_duration {
+    use super::get_step_duration;
+
+    #[test]
+    fn test() {
+        assert_eq!(get_step_duration('A'), 1 + 60);
+        assert_eq!(get_step_duration('M'), 13 + 60);
+        assert_eq!(get_step_duration('Z'), 26 + 60);
     }
 }
 
