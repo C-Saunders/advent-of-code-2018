d3abe05c618cb82df7d212e27ea310e59cddd1d2
Charlie Saunders <charlieasaunders@gmail.com>
2019-01-06 21:51:18 -0600

    WIP.

--- problem8/src/main.rs
+++ problem8/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn get_as_array(_input: &str) {
+    
+}
+
+fn process_child() {
+    let num_children = 0;
+
+    let mut running_metadata_total = 0;
+    let mut running_len_total = 0;
+
+    for _ in num_children {
+        let start_pos = running_len_total + 0;
+    }
+
+}
