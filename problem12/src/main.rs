ba605cb20e97b4a0608ff50cfaf747b26d50c571
Charlie Saunders <charlieasaunders@gmail.com>
2019-02-03 17:55:47 -0500

    WIP.

--- problem12/src/main.rs
+++ problem12/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod parser;
+
+fn main() -> Result<(), String> {
+    let input = fs::read_to_string("input/data.txt").map_err(|e| e.to_string())?;
+    let rules = 
+
+    let initial_state = "#.##.#.##..#.#...##...#......##..#..###..##..#.#.....##..###...#.#..#...######...#####..##....#..###";
+
+
 }
+
+
